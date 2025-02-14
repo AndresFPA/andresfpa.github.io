cc98e6030b7bfaa872865be6d3555238119d2772
Andres Felipe Perez <77491321+AndresFPA@users.noreply.github.com>
2025-02-14 16:06:36 +0100

    Delete user-data directory

--- user-data/urls.js
+++
@@ -1,9 +0,0 @@
-const githubUsername = "andresperez96";
-
-const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
-const gitRepos = (username) => `https://api.github.com/users/${username}/repos`;
-
-export const URLs = {
-    gitConnected: createGitConnectedURL(githubUsername),
-    gitRepo: gitRepos(githubUsername),
-};
